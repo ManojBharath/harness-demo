7141f49a503f4feda9d033d6b2a529be6938cb44
ManojBharath <85289458+ManojBharath@users.noreply.github.com>
2026-04-14 10:27:09 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Harness CI POC running on Java 17 ✅");
+        System.out.println("Runtime Version: " + Runtime.version());
+    }
+}
